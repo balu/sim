2e38fce271a732eaa91fdf3dfb59515a64119533
bkomarath <bkomarath@rbgo.in>
2023-01-14 11:36:36 +0530

    Booleans and IfElse.

--- sim.py
+++ sim.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
 from fractions import Fraction
-from typing import Union, Mapping
+from typing import Union, Mapping, cast
 
 @dataclass
 class NumLiteral:
     value: Fraction
     def __init__(self, *args):
         self.value = Fraction(*args)
+
+@dataclass
+class BoolLiteral:
+    value: bool
 
 @dataclass
 class BinOp:
@@ -24,9 +28,15 @@ class Let:
     e1: 'AST'
     e2: 'AST'
 
-AST = NumLiteral | BinOp | Variable | Let
+@dataclass
+class IfElse:
+    condition: 'AST'
+    iftrue: 'AST'
+    iffalse: 'AST'
 
-Value = Fraction
+AST = NumLiteral | BoolLiteral | BinOp | Variable | Let | IfElse
+
+Value = Fraction | bool
 
 class InvalidProgram(Exception):
     pass
@@ -37,6 +47,8 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
     match program:
         case NumLiteral(value):
             return value
+        case BoolLiteral(value):
+            return value
         case Variable(name):
             if name in environment:
                 return environment[name]
@@ -44,14 +56,60 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
         case Let(Variable(name), e1, e2):
             v1 = eval(e1, environment)
             return eval(e2, environment | { name: v1 })
+        case IfElse(condition, iftrue, iffalse):
+            c = eval(condition, environment)
+            match c:
+                case bool(True):
+                    return eval(iftrue, environment)
+                case bool(False):
+                    return eval(iffalse, environment)
+            raise InvalidProgram()
         case BinOp("+", left, right):
-            return eval(left, environment) + eval(right, environment)
+            match eval(left, environment), eval(right, environment):
+                case [Fraction() as left, Fraction() as right]:
+                    return left + right
+            raise InvalidProgram()
         case BinOp("-", left, right):
-            return eval(left, environment) - eval(right, environment)
+            match eval(left, environment), eval(right, environment):
+                case [Fraction() as left, Fraction() as right]:
+                    return left - right
+            raise InvalidProgram()
         case BinOp("*", left, right):
-            return eval(left, environment) * eval(right, environment)
+            match eval(left, environment), eval(right, environment):
+                case [Fraction() as left, Fraction() as right]:
+                    return left * right
+            raise InvalidProgram()
         case BinOp("/", left, right):
-            return eval(left, environment) / eval(right, environment)
+            match eval(left, environment), eval(right, environment):
+                case [Fraction() as left, Fraction() as right]:
+                    if right == 0:
+                        raise InvalidProgram()
+                    return left / right
+            raise InvalidProgram()
+        case BinOp("=", left, right):
+            return eval(left, environment) == eval(right, environment)
+        case BinOp("≠", left, right):
+            return eval(left, environment) != eval(right, environment)
+        case BinOp(">", left, right):
+            match eval(left, environment), eval(right, environment):
+                case [Fraction() as left, Fraction() as right]:
+                    return left > right
+            raise InvalidProgram()
+        case BinOp("<", left, right):
+            match eval(left, environment), eval(right, environment):
+                case [Fraction() as left, Fraction() as right]:
+                    return left < right
+            raise InvalidProgram()
+        case BinOp("≥", left, right):
+            match eval(left, environment), eval(right, environment):
+                case [Fraction() as left, Fraction() as right]:
+                    return left >= right
+            raise InvalidProgram()
+        case BinOp("≤", left, right):
+            match eval(left, environment), eval(right, environment):
+                case [Fraction() as left, Fraction() as right]:
+                    return left <= right
+            raise InvalidProgram()
     raise InvalidProgram()
 
 def test_eval():
@@ -79,3 +137,14 @@ def test_let_eval():
     e3 = NumLiteral(6)
     e  = BinOp("+", Let(a, e1, e2), Let(a, e3, e2))
     assert eval(e) == 22
+    pi = Variable("π")
+    piv = NumLiteral(22, 7)
+    e = Let(pi, piv, BinOp("*", pi, BinOp("*", NumLiteral(7), NumLiteral(7))))
+    assert eval(e) == 22*7
+
+def test_ifelse_eval():
+    c = BinOp(">", NumLiteral(5), NumLiteral(3))
+    t = BinOp("-", NumLiteral(5), NumLiteral(3))
+    f = BinOp("+", NumLiteral(5), NumLiteral(3))
+    e = IfElse(c, t, f)
+    assert eval(e) == 2
