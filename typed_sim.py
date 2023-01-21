322c9d0d3279823887b9cb96cd8a6939278e9939
bkomarath <bkomarath@rbgo.in>
2023-01-21 12:15:43 +0530

    Lexer.

--- typed_sim.py
+++ typed_sim.py
@@ -4,6 +4,120 @@ from typing import Optional, NewType
 
 # A minimal example to illustrate typechecking.
 
+class EndOfStream(Exception):
+    pass
+
+@dataclass
+class Stream:
+    source: str
+    pos: int
+
+    def from_string(s):
+        return Stream(s, 0)
+
+    def next_char(self):
+        if self.pos >= len(self.source):
+            raise EndOfStream()
+        self.pos = self.pos + 1
+        return self.source[self.pos - 1]
+
+    def unget(self):
+        assert self.pos > 0
+        self.pos = self.pos - 1
+
+# Define the token types.
+
+@dataclass
+class Num:
+    n: int
+
+@dataclass
+class Bool:
+    b: bool
+
+@dataclass
+class Keyword:
+    word: str
+
+@dataclass
+class Identifier:
+    word: str
+
+@dataclass
+class Operator:
+    op: str
+
+Token = Num | Bool | Keyword | Identifier | Operator
+
+class EndOfTokens(Exception):
+    pass
+
+keywords = "if then else end".split()
+symbolic_operators = "+ - × / < > <= >= = ≠".split()
+word_operators = "and or not quot rem"
+whitespace = " \t\n"
+
+def word_to_token(word):
+    if word in keywords:
+        return Keyword(word)
+    if word in word_operators:
+        return Operator(word)
+    if word == "True":
+        return Bool(True)
+    if word == "False":
+        return Bool(False)
+    return Identifier(word)
+
+@dataclass
+class Lexer:
+    stream: Stream
+
+    def from_stream(s):
+        return Lexer(s)
+
+    def next_token(self) -> Token:
+        try:
+            match self.stream.next_char():
+                case c if c in symbolic_operators: return Operator(c)
+                case c if c.isdigit():
+                    n = int(c)
+                    while True:
+                        try:
+                            c = self.stream.next_char()
+                            if c.isdigit():
+                                n = n*10 + int(c)
+                            else:
+                                self.stream.unget()
+                                return Num(n)
+                        except EndOfStream:
+                            return Num(n)
+                case c if c.isalpha():
+                    s = c
+                    while True:
+                        try:
+                            c = self.stream.next_char()
+                            if c.isalpha():
+                                s = s + c
+                            else:
+                                self.stream.unget()
+                                return word_to_token(s)
+                        except EndOfStream:
+                            return word_to_token(s)
+                case c if c in whitespace:
+                    return self.next_token()
+        except EndOfStream:
+            raise EndOfTokens
+ 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return self.next_token()
+        except EndOfTokens:
+            raise StopIteration
+
+
 @dataclass
 class NumType:
     pass
@@ -52,7 +166,7 @@ def typecheck(program: AST, env = None) -> TypedAST:
             return t
         case BoolLiteral() as t: # already typed.
             return t
-        case BinOp(op, left, right) if op in ["+", "*"]:
+        case BinOp(op, left, right) if op in "+×-/":
             tleft = typecheck(left)
             tright = typecheck(right)
             if tleft.type != NumType() or tright.type != NumType():
@@ -88,4 +202,8 @@ def test_typecheck():
     te = typecheck(BinOp("<", NumLiteral(2), NumLiteral(3)))
     assert te.type == BoolType()
     with pytest.raises(TypeError):
-        typecheck(BinOp("+", BinOp("*", NumLiteral(2), NumLiteral(3)), BinOp("<", NumLiteral(2), NumLiteral(3))))
+        typecheck(BinOp("+", BinOp("×", NumLiteral(2), NumLiteral(3)), BinOp("<", NumLiteral(2), NumLiteral(3))))
+
+L = Lexer.from_stream(Stream.from_string("if 22 > 33 then 5+3 else e × x end"))
+for t in L:
+    print(t)
